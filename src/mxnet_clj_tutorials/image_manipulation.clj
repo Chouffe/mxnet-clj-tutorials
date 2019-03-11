4ef9bd94b814dca3a82612d47da1147e04acf4c8
Chouffe <chouffe.caillau@gmail.com>
2019-03-11 21:29:26 +0100

    [image-manipulation] refactor image manipulation tutorial

--- src/mxnet_clj_tutorials/image_manipulation.clj
+++ src/mxnet_clj_tutorials/image_manipulation.clj
@@ -1,4 +1,5 @@
 (ns mxnet-clj-tutorials.image-manipulation
+  "Image manipulation tutorial."
   (:require
     [clojure.java.io :as io]
 
@@ -6,9 +7,10 @@
     [org.apache.clojure-mxnet.ndarray :as ndarray]
     [org.apache.clojure-mxnet.shape :as mx-shape]
 
+    [opencv4.colors.rgb :as rgb]
+    [opencv4.mxnet :as mx-cv]
     [opencv4.core :as cv]
     [opencv4.utils :as cvu])
-
   (:import org.opencv.core.Mat java.awt.image.DataBufferByte))
 
 (defn download!
@@ -20,10 +22,9 @@
 
 (defn preview!
   "Preview image from `filename` and display it on the screen in a new window
-
-  >>> (preview! \"images/cat.jpg\")
-  >>> (preview! \"images/cat.jpg\" :h 300 :w 200)
-  "
+   Ex:
+    (preview! \"images/cat.jpg\")
+    (preview! \"images/cat.jpg\" :h 300 :w 200)"
   ([filename]
    (preview! filename {:h 400 :w 400}))
   ([filename {:keys [h w]}]
@@ -34,20 +35,18 @@
 
 (defn preprocess-mat
   "Preprocessing steps on a `mat` from OpenCV.
-  Example of commons preprocessing tasks"
+   Example of commons preprocessing tasks"
   [mat]
   (-> mat
-      ;; Substract mean
+      ;; Subtract mean
       (cv/add! (cv/new-scalar 103.939 116.779 123.68))
       ;; Resize
       (cv/resize! (cv/new-size 400 400))
       ;; Maps pixel values from [-125, 125] to [0, 250]
       (cv/convert-to! cv/CV_8SC3 0.5)))
-      ;; TODO: add cropping?
-
 
 (defn mat->ndarray
-  "Converts a `mat` from OpenCV to an MXNET `ndarray`"
+  "Convert a `mat` from OpenCV to an MXNet `ndarray`"
   [mat]
   (let [h (.height mat)
         w (.width mat)
@@ -71,11 +70,32 @@
 
   `filename`: string representing the image on disk
   `shape-vec`: is the actual shape of the returned `ndarray`
-  "
+   return: ndarray"
   [filename shape-vec]
   (-> filename
       cv/imread
       mat->ndarray))
+
+(defn draw-bounding-box!
+  "Draw bounding box on `img` given the `top-left` and `bottom-right` coordonates.
+  Add `label` when provided.
+  returns: nil"
+  [img {:keys [label top-left bottom-right]}]
+  (let [[x0 y0] top-left
+        [x1 y1] bottom-right
+        top-left-point (cv/new-point x0 y0)
+        bottom-right-point (cv/new-point x1 y1)]
+    (cv/rectangle img top-left-point bottom-right-point rgb/white 1)
+    (when label
+      (cv/put-text! img label top-left-point cv/FONT_HERSHEY_DUPLEX 1.0 rgb/white 1))))
+
+(defn draw-predictions!
+    "Draw all predictions on an `img` passing `results` which is a collection
+     of bounding boxes data.
+     returns: nil"
+    [img results]
+    (doseq [{:keys [label top-left bottom-right] :as result} results]
+      (draw-bounding-box! img result)))
 
 (comment
 
@@ -95,35 +115,67 @@
       cvu/imshow))
 
 (comment
+  ;; Writing image to disk
   (-> "images/dog.jpg"
-      ;;
-      (mx-img/read-image #_{:to-rgb true}) ;; optiona to-rgb, true by default
+      ;; Convert filename to NDArray
+      (mx-img/read-image {:to-rgb true}) ;; option :to-rgb, true by default
       ;; Resizing image to height = 400, width = 400
       (mx-img/resize-image 400 400)
+      ;; Convert to BufferedImage
       mx-img/to-image
-      ;; Saving image to disk
+      ;; Saving BufferedImage to disk
       (javax.imageio.ImageIO/write "jpg" (java.io.File. "test2.jpg")))
 
+  ;; Writing image to disk
   (-> "images/mnist_digit_8.jpg"
+      ;; Load image from disk
       (mx-img/read-image {:to-rgb true})
-      ; mx-img/to-image
-      ; (javax.imageio.ImageIO/write "jpg" (java.io.File. "test.jpg"))
-      ; cvu/buffered-image-to-mat
-      ;; Save Image To File
+      ;; Convert NDArray to Mat
       ndarray->mat
+      ;; Save Image to disk
       (cv/imwrite "test-digit.jpg"))
       ; cvu/imshow
 
-
-  ;; Showing an image using `cvu/buffered-image-to-mat`
+  ;; Showing an image using `buffered-image-to-mat`
   (-> "images/dog.jpg"
+      ;; Read image from disk
       (mx-img/read-image {:to-rgb true})
+      ;; Convert to BufferedImage - Can be very slow...
       mx-img/to-image
+      ;; Convert to Mat
       cvu/buffered-image-to-mat
+      ;; Show Mat
       cvu/imshow)
 
-  ;; Much Faster using custom `ndarray->mat`
+  ;; Showing an image using `ndarray->mat`
   (-> "images/dog.jpg"
+      ;; Read image from disk
       (mx-img/read-image {:to-rgb false})
+      ;; Convert NDArray to Mat
       ndarray->mat
-      cvu/imshow))
+      ;; Show Mat
+      cvu/imshow)
+
+  ;; Showing an image using `mx-cv/ndarray-to-mat` from `origami`
+  (-> "images/dog.jpg"
+      ;; Read image from disk
+      (mx-img/read-image {:to-rgb false})
+      ;; Convert NDArray to Mat
+      mx-cv/ndarray-to-mat
+      ;; Show Mat
+      cvu/imshow)
+
+  ;; Drawing one bounding box on an image of dog
+  (let [img (cv/imread "images/dog.jpg")]
+    (draw-bounding-box! img {:top-left [200 440]
+                             :bottom-right [350 525]
+                             :label "cookie"})
+    (cvu/imshow img))
+
+  ;; Drawing multiple bounding boxes on an image of dog
+  (let [img (cv/imread "images/dog.jpg")
+        results [{:top-left [200 70] :bottom-right [830 430] :label "dog"}
+                 {:top-left [200 440] :bottom-right [350 525] :label "cookie"}]]
+    (draw-predictions! img results)
+    (cvu/imshow img))
+  )
