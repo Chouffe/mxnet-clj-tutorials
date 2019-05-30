b0d14edf6e853be55f97cb8378cf461f3c4a04fa
Chouffe <chouffe.caillau@gmail.com>
2019-05-30 17:30:08 +0200

    Add get resnet 50 script

---
+++ scripts/models/get_resnet_50.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -evx
+
+PROJECT_ROOT=$(cd "$(dirname $0)/../.."; pwd)
+
+model_path=$PROJECT_ROOT/model/
+
+if [ ! -f "$model_path/resnet-50-0000.params" ]; then
+  wget http://data.mxnet.io/models/imagenet/resnet/50-layers/resnet-50-symbol.json -P $model_path
+  wget http://data.mxnet.io/models/imagenet/resnet/50-layers/resnet-50-0000.params -P $model_path
+  wget http://data.mxnet.io/models/imagenet/resnet/50-layers/synset.txt -P $model_path
+fi
+
