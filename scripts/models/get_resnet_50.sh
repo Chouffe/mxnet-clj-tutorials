d592f1ac02e326fa9af1c725fe5ef8a37797c781
Chouffe <chouffe.caillau@gmail.com>
2019-05-30 17:39:28 +0200

    fix synset.txt

--- scripts/models/get_resnet_50.sh
+++ scripts/models/get_resnet_50.sh
@@ -9,6 +9,6 @@ model_path=$PROJECT_ROOT/model/
 if [ ! -f "$model_path/resnet-50-0000.params" ]; then
   wget http://data.mxnet.io/models/imagenet/resnet/50-layers/resnet-50-symbol.json -P $model_path
   wget http://data.mxnet.io/models/imagenet/resnet/50-layers/resnet-50-0000.params -P $model_path
-  wget http://data.mxnet.io/models/imagenet/resnet/50-layers/synset.txt -P $model_path
+  wget http://data.mxnet.io/models/imagenet/resnet/synset.txt -P $model_path
 fi
 
